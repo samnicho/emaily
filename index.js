7d8b33e63212b503e94d28560a7208d58b82be20
Sam Nicholson <samnicho@sams-mbp.home>
2020-05-06 18:03:59 +0100

    finished auth flow

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const passport = require('passport');
 const keys = require('./config/keys');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 
-// ***--- EXPRESS & MIDDLEWARE in EXPRESS ---***
+// ***--- EXPRESS & MIDDLEWARE in EXPRESS ---***                                                                      
 //
+// 'use' indicates middleware
+// middleware will execute before the route handlers
 //                                                                                  |-->   put  -----|
 //                            express()                                             |                |
 //                               |                                                  |-->   delete  --|
@@ -18,27 +22,21 @@ const app = express();
 //                                                                                  |                |
 //                                                                                  |-->   get  -----|
 
-// 'use' indicates middleware 
-// middleware will execute before the route handlers
-
-
 // --- tell app to use cookies
 // cookies will be used with passport for authentication
 app.use(
-  // configure cookieSession lib
+  // configure cookieSession library
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // cookies expire after 30 days (value in milliseconds)
     keys: [keys.cookieKey]
   })
 );
 
-// tell passport to use cookies
+// --- tell passport to use cookies
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(keys.mongoURI);
-
-require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -3,56 +3,62 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy; // provides 
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-// !! do not use require for mongoose models as it will throw an error, particularly in some testing environments
+// !! do not use require for mongoose models as it will throw an error, particularly when running some testing libraries (Jest, Mocha, etc.)
 // this is if the model is required multiple times, the application thinks that multiple models (all called 'user') are being loaded
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
-// call serializeUser with the user as a mongoose user model in order to generate the identifying piece of info
+// --- Serialize user
+// call serializeUser with the mongoose user model in order to generate the identifying piece of info
+// this identifying piece of info will be the document id for this user from the db
 // args:
 // 1. the user 
 // 2. a callback which takes two args, the first is an error obj or string and the second is a user
 passport.serializeUser((user, done) => {
   // user the user id provided by mongo, not the profile id returned by google
   // this is because there could be multiple 'Strategies' used 
+  console.log('serialize user: ');
+  console.log(user);
   done(null, user.id)
 });
 
+// --- Deserialize user
 // call deserializeUser with the id to generate a mongoose user model
+// the id will be used to search the db for a user document with a matching id
 // args:
 // 1. the user id we used in serializeUser
-// 2. a callback which takes two args, the first is an error obj or string and the second is a user
+// 2. a callback which takes two args (1. an error obj or string, 2. a user)
 passport.deserializeUser((id, done) => {
-  console.log(id);
+  console.log('deserializeUser: ');
+  console.log('id: ' + id);
   
   User.findById(id).then((user) => {
-    // console.log(id);
-    // console.log(user);
+    console.log('user:', user);
     done(null, user);
-  })
+  });
 });
 
-//configure passport to use Google auth and confiugure GoogleStrategy
+// configure passport to use Google auth and configure GoogleStrategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback", // the route that users will be sent to once they've granted permissions via google (can be anything)
-      proxy: true // tell GoogleStrategy to trust proxys, this fixes the https to http error
+      callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
+      // console.log('accessToken: ', accessToken);
+      console.log('profile ID: ', profile.id);
+      console.log('profile name: ', profile.displayName);
       // check the DB for a user record with the same profile ID as the one just returned by google
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
+          console.log('existing user');
           // we already have a record with the given profile ID
-          console.log('existing');
-          console.log(existingUser);
           done(null, existingUser);
         } else {
+          console.log('new user');
           // we don't have a user record witha this ID, make a new record and save it to the DB
           new User({ googleId: profile.id }).save().then((user) => {
-            console.log('new');
-            console.log(user);
             done(null, user);
           });
         }
